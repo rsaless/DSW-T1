0ccdb73974765aa186f69a9f22360dead83099f3
rsaless <rspavarina@gmail.com>
2019-04-16 03:33:44 -0300

    Início da implementação dos templates e servlets relacionados aos teatros

--- DSW-T1/src/java/Controllers/TeatroServlet.java
+++ DSW-T1/src/java/Controllers/TeatroServlet.java
@@ -1,86 +1,111 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controllers;
 
+import DAO.TeatroDAO;
+import Models.Teatro;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author rafap
- */
-public class CadastraTeatro extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+@WebServlet(urlPatterns = "/teatro")
+public class TeatroServlet extends HttpServlet {
+    private TeatroDAO dao;
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CadastraTeatro</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CadastraTeatro at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        throws ServletException, IOException {
+        String action = request.getServletPath();
+        try {
+            switch (action){
+                case "/cadastro": apresentaFormCadastro(request, response); break;
+                // case "/insercao": insere(request, response); break;
+                case "/remocao": remove(request, response); break;
+                case "/edicao": apresentaFormEdicao(request, response); break;
+                // case "/atualizacao": atualize(request, response); break;
+                default: lista(request, response); break;
+            }
+        } catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
         }
+        
+    }
+    
+    private void lista(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Teatro> teatros = dao.listar();
+        request.setAttribute("listaTeatros", teatros);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("teatro/listaTeatros.jsp");
+        dispatcher.forward(request, response);
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private void apresentaFormCadastro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("teatro/formTeatro.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void apresentaFormEdicao(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Teatro teatro = dao.get(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("teatro/formTeatro.jsp");
+        request.setAttribute("teatro", teatro);
+        dispatcher.forward(request, response);
+    }
+    
+    /*
+    private void insere(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String titulo = request.getParameter("titulo");
+        String autor = request.getParameter("autor");
+        Integer ano = Integer.parseInt(request.getParameter("ano"));
+        Float preco = Float.parseFloat(request.getParameter("preco"));
+
+        Livro livro = new Livro(titulo, autor, ano, preco);
+        dao.insert(livro);
+        response.sendRedirect("lista");
+    }
+    */
+    /*
+    private void atualize(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        String titulo = request.getParameter("titulo");
+        String autor = request.getParameter("autor");
+        Integer ano = Integer.parseInt(request.getParameter("ano"));
+        Float preco = Float.parseFloat(request.getParameter("preco"));
+
+        Livro livro = new Livro(id, titulo, autor, ano, preco);
+        dao.update(livro);
+        response.sendRedirect("lista");
+    }
+    */
+
+    private void remove(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        Teatro teatro = new Teatro(id);
+        dao.deletar(teatro);
+        response.sendRedirect("lista");
+    }
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    public void init() {
+        dao = new TeatroDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
--- DSW-T1/web/teatro/listaTeatros.jsp
+++ DSW-T1/web/teatro/listaTeatros.jsp
@@ -1,15 +1,55 @@
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/bootstrap.css">
-        <title>JSP Page</title>
+        <title>Listagem de Teatros</title>
     </head>
     <body>
-        <h1>Lista de teatros</h1>
+        <center>
+            <h1>TEATROS</h1>
+            <h2>
+                <a href="adicionar_teatro">Adicionar novo Teatro </a>|
+                <a href="lista_teatros">Lista de Teatros</a>
+            </h2>
+        </center>
+        <div align="center">
+            <h2>Lista de Teatros</h2>
+            <table border="1" cellpadding="5">
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Senha</th>
+                    <th>Cidade</th>
+                    <th>CNPJ</th>
+                    <th>Acões</th>
+                </tr>
+                <c:forEach var="teatro" items="${requestScope.listaTeatros}">
+                    <tr>
+                        <td><c:out value="${teatro.id}" /></td>
+                        <td><c:out value="${teatro.nome}" /></td>
+                        <td><c:out value="${teatro.email}" /></td>
+                        <td><c:out value="${teatro.senha}" /></td>
+                        <td><c:out value="${teatro.cidade}" /></td>
+                        <td><c:out value="${teatro.cnpj}" /></td>
+                        <td>
+                            <a href="edicao?id=<c:out value='${teatro.id}' />">Edição</a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="remocao?id=<c:out value='${teatro.id}' />" 
+                               onclick="return confirm('Tem certeza de que deseja excluir este item?');">
+                                Remoção
+                            </a>                    	
+                        </td>
+                    </tr>
+                </c:forEach>
+            </table>
+        </div>	
     </body>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 </html>
+
