cfb5bacc89cdf475df08140293a86f88301c6ff2
Unknown <guiffranco1@gmail.com>
2019-04-21 12:12:35 -0300

    Cria Servlet do site

--- DSW-T1/src/java/Controllers/SiteServlet.java
+++ DSW-T1/src/java/Controllers/SiteServlet.java
@@ -1,86 +1,128 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controllers;
 
+import DAO.SiteDAO;
+import Models.Site;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author rafap
- */
+@WebServlet(urlPatterns = "/site/*")
 public class SiteServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
+    private SiteDAO dao;
+    
+    @Override
+    public void init() {
+        dao = new SiteDAO();
+    }
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CadastraSite</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CadastraSite at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        throws ServletException, IOException {
+        String action = request.getPathInfo();
+        if(action == null) action = "";
+        try {
+            switch (action){
+                case "/cadastro": 
+                    apresentaFormCadastro(request, response); 
+                    break;
+                case "/insercao": 
+                    insere(request, response); 
+                    break;
+                case "/remocao": 
+                    remove(request, response); 
+                    break;
+                case "/edicao": 
+                    apresentaFormEdicao(request, response); 
+                    break;
+                case "/atualizacao": 
+                    atualize(request, response); 
+                    break;
+                default: 
+                    lista(request, response); 
+                    break;
+            }
+        } catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
         }
+        
+    }
+    
+    private void lista(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Site> sites = dao.listar();
+        request.setAttribute("listaSites", sites);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/templates_site/listaSites.jsp");
+        dispatcher.forward(request, response);
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private void apresentaFormCadastro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/templates_site/formSite.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void apresentaFormEdicao(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Site site = dao.get(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/templates_site/formSite.jsp");
+        request.setAttribute("site", site);
+        dispatcher.forward(request, response);
+    }
+    
+    
+    private void insere(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        request.setCharacterEncoding("UTF-8");
+        String email = request.getParameter("email");
+        String senha = request.getParameter("senha");
+        String url = request.getParameter("url");
+        String nome = request.getParameter("nome");
+        Integer telefone = Integer.parseInt(request.getParameter("telefone"));
+
+        Site site = new Site(email, senha, url, nome, telefone);
+        dao.inserir(site);
+        response.sendRedirect("/DSW-T1/site");
+    }
+    
+    
+    private void atualize(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        String email = request.getParameter("email");
+        String senha = request.getParameter("senha");
+        String url = request.getParameter("url");
+        String nome = request.getParameter("nome");
+        Integer telefone = Integer.parseInt(request.getParameter("telefone"));
+
+        Site site = new Site(email, senha, url, nome, telefone, id);
+        dao.atualizar(site);
+        response.sendRedirect("/DSW-T1/site");
+    }
+    
+
+    private void remove(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        Site site = new Site(id);
+        dao.deletar(site);
+        response.sendRedirect("/DSW-T1/site");
+    }
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
