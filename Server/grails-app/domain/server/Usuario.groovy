cdb96d8de30f17cd6debcc94a0f5907333547a69
rsaless <rspavarina@gmail.com>
2019-06-05 01:15:11 -0300

    Criação das classes de domínio

---
+++ Server/grails-app/domain/server/Usuario.groovy
@@ -0,0 +1,9 @@
+package server
+
+
+import grails.rest.*
+
+@Resource(readOnly = false, formats = ['json', 'xml'])
+class Usuario {
+
+}
---
+++ Server/src/test/groovy/server/PromocaoSpec.groovy
@@ -0,0 +1,18 @@
+package server
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PromocaoSpec extends Specification implements DomainUnitTest<Promocao> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+        true == false
+    }
+}
