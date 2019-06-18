da517d9604381bc3e42c11791305aa5ef1bf7f8a
rsaless <rspavarina@gmail.com>
2019-06-18 00:32:34 -0300

    Criação dos componentes necessários

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,12 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SitesComponent } from './components/sites/sites.component';
 import { SiteCadastroComponent } from './components/site-cadastro/site-cadastro.component';
+import { SiteDetalhesComponent } from './components/site-detalhes/site-detalhes.component';
+import { SiteEdicaoComponent } from './components/site-edicao/site-edicao.component';
+import { TeatroDetalhesComponent } from './components/teatro-detalhes/teatro-detalhes.component';
+import { TeatroEdicaoComponent } from './components/teatro-edicao/teatro-edicao.component';
+import { TeatroCadastroComponent } from './components/teatro-cadastro/teatro-cadastro.component';
+import { TeatrosComponent } from './components/teatros/teatros.component';
+import { PromocaoCadastroComponent } from './components/promocao-cadastro/promocao-cadastro.component';
+import { PromocaoDetalhesComponent } from './components/promocao-detalhes/promocao-detalhes.component';
+import { PromocaoEdicaoComponent } from './components/promocao-edicao/promocao-edicao.component';
+import { PromocoesComponent } from './components/promocoes/promocoes.component';
+import { PromocoesSiteComponent } from './components/promocoes-site/promocoes-site.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SitesComponent,
-    SiteCadastroComponent
+    SiteCadastroComponent,
+    SiteDetalhesComponent,
+    SiteEdicaoComponent,
+    TeatroDetalhesComponent,
+    TeatroEdicaoComponent,
+    TeatroCadastroComponent,
+    TeatrosComponent,
+    PromocaoCadastroComponent,
+    PromocaoDetalhesComponent,
+    PromocaoEdicaoComponent,
+    PromocoesComponent,
+    PromocoesSiteComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ Client/src/app/components/promocao-edicao/promocao-edicao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promocao-edicao',
+  templateUrl: './promocao-edicao.component.html',
+  styleUrls: ['./promocao-edicao.component.css']
+})
+export class PromocaoEdicaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
