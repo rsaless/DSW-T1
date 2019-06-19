419a1d12e33d21fc09db326b32395ae23dcc3cc1
Rafael Sales Pavarina <rspavarina@gmail.com>
2019-06-19 18:36:03 -0300

    Cadastro de Teatros, Sites e Promoções

--- Client/src/app/components/promocao-cadastro/promocao-cadastro.component.ts
+++ Client/src/app/components/promocao-cadastro/promocao-cadastro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { Site } from '../../models/site';
+import { Teatro } from '../../models/teatro';
 
 @Component({
   selector: 'app-promocao-cadastro',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromocaoCadastroComponent implements OnInit {
 
-  constructor() { }
+  promocaoForm: FormGroup;
+  sites: Site[];
+  teatros: Teatro[];
+  isLoadingResults = false;
+
+  constructor(
+    private router: Router, 
+    private api: ApiService, 
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.promocaoForm = this.formBuilder.group({
+      nome_peca: [null, Validators.required],
+      preco: [0.01, Validators.required],
+      cnpj: [null, Validators.required],
+      dia_hora: [null, Validators.required],
+      nome_site: [null, Validators.required],
+      site: [null, Validators.required],
+      teatro: [null, Validators.required]
+    });
+    this.getData();
   }
+
+  async getData() {
+    this.sites = await this.api.getSites().toPromise();
+    this.teatros = await this.api.getTeatros().toPromise();
+    this.isLoadingResults = false;
+    console.debug('No issues, I will wait until promise is resolved..');
+  }
+
+  onFormSubmit(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.addPromocao(form)
+      .subscribe(res => {
+        let id = res['id'];
+        this.isLoadingResults = false;
+        this.router.navigate(['/promocao-detalhes', id]);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
+  }
+
+
 
 }
--- Client/src/app/components/site-cadastro/site-cadastro.component.ts
+++ Client/src/app/components/site-cadastro/site-cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-site-cadastro',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteCadastroComponent implements OnInit {
 
-  constructor() { }
+  siteForm: FormGroup;
+  isLoadingResults = false;
+
+  constructor(
+    private router: Router, 
+    private api: ApiService, 
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.siteForm = this.formBuilder.group({
+      nome: [null, Validators.required],
+      email: [null, Validators.required],
+      senha: [null, Validators.required],
+      url: [null, Validators.required],
+      telefone: [33333333, Validators.required],
+    });
+  }
+
+  onFormSubmit(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.addSite(form)
+      .subscribe(res => {
+        let id = res['id'];
+        this.isLoadingResults = false;
+        this.router.navigate(['/site-detalhes', id]);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
