76f35b0c985d8aff25276020e89cb414a3845efe
Rafael Sales Pavarina <rspavarina@gmail.com>
2019-06-19 02:29:28 -0300

    Detalhes de Teatros, Sites e Promoções

--- Client/src/app/components/teatro-detalhes/teatro-detalhes.component.ts
+++ Client/src/app/components/teatro-detalhes/teatro-detalhes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { Teatro } from '../../models/teatro'
 
 @Component({
   selector: 'app-teatro-detalhes',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeatroDetalhesComponent implements OnInit {
 
-  constructor() { }
+  teatro: Teatro = {
+    id: '',
+    nome: '',
+    email: '',
+    senha: '',
+    cidade: '',
+    cnpj: '',
+  };
+  isLoadingResults = true;
 
+  constructor(
+    private route: ActivatedRoute, 
+    private api: ApiService, 
+    private router: Router
+  ) { }
+
+  async getData(id) {
+    this.teatro = await this.api.getTeatro(id).toPromise();
+    this.isLoadingResults = false;
+    console.debug('No issues, I will wait until promise is resolved..');
+  }
   ngOnInit() {
+    this.getData(this.route.snapshot.params['id']);
+  }
+  deleteTeatro(id) {
+    this.isLoadingResults = true;
+    this.api.deleteTeatro(id)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/teatros']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
   }
 
 }
