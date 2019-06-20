5eb46f82d8a3113040deb59641ef194149ca5a0c
Rafael Sales Pavarina <rspavarina@gmail.com>
2019-06-20 18:55:39 -0300

    Edição de Teatros, sites e Promoções

--- Client/src/app/components/promocao-edicao/promocao-edicao.component.ts
+++ Client/src/app/components/promocao-edicao/promocao-edicao.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { Teatro } from '../../models/teatro';
+import { Site } from '../../models/site';
+import { Promocao } from 'src/app/models/promocao';
 
 @Component({
   selector: 'app-promocao-edicao',
@@ -7,9 +13,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromocaoEdicaoComponent implements OnInit {
 
-  constructor() { }
+  promocaoForm: FormGroup;
+  id: string = '';
+  isLoadingResults = true;
+  selected_site: Site = null;
+  selected_teatro: Teatro = null;
+  sites: Site[];
+  teatros: Teatro[];
+
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute, 
+    private api: ApiService, 
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.isLoadingResults = true;
+    this.promocaoForm = this.formBuilder.group({
+      nome_peca: [null, Validators.required],
+      preco: [null, Validators.required],
+      cnpj: [null, Validators.required],
+      dia_hora: [null, Validators.required],
+      nome_site: [null, Validators.required],
+      site: [new Site(), Validators.required],
+      teatro: [new Teatro(), Validators.required]
+    });
+    this.getData(this.route.snapshot.params['id']);
   }
+
+  async getData(id) {
+    this.sites = await this.api.getSites().toPromise();
+    this.teatros = await this.api.getTeatros().toPromise();
+    let promocao: Promocao = await this.api.getPromocao(id).toPromise();
+    this.id = promocao.id;
+    this.promocaoForm.setValue({
+      nome_peca: promocao.nome_peca,
+      preco: promocao.preco,
+      cnpj: promocao.cnpj,
+      dia_hora: promocao.dia_hora,
+      nome_site: promocao.nome_site,
+      site: promocao.site,
+      teatro: promocao.teatro
+    });
+    this.selected_site = promocao.site;
+    this.selected_teatro = promocao.teatro;
+    this.isLoadingResults = false;
+    console.debug('No issues, I will wait until promise is resolved..');
+  }
+
+  onFormSubmit(form:NgForm) {
+    this.isLoadingResults = true;
+    this.api.updatePromocao(this.id, form)
+      .subscribe(res => {
+          let id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/promocao-detalhes', id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
+
+  promocaoDetalhes() {
+    this.router.navigate(['/promocao-detalhes', this.id]);
+  }
+  
+
 
 }
--- Client/src/app/components/site-edicao/site-edicao.component.ts
+++ Client/src/app/components/site-edicao/site-edicao.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { Site } from 'src/app/models/site';
 
 @Component({
   selector: 'app-site-edicao',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteEdicaoComponent implements OnInit {
 
-  constructor() { }
+  siteForm: FormGroup;
+  id: string = '';
+  isLoadingResults = true;
+
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute, 
+    private api: ApiService, 
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.isLoadingResults = true;
+    this.siteForm = this.formBuilder.group({
+      nome: [null, Validators.required],
+      email: [null, Validators.required],
+      senha: [null, Validators.required],
+      url: [null, Validators.required],
+      telefone: [null, Validators.required],
+    });
+    this.getData(this.route.snapshot.params['id']);
+  }
+
+  async getData(id) {
+    let site: Site = await this.api.getSite(id).toPromise();
+    this.id = site.id;
+    this.siteForm.setValue({
+      nome: site.nome,
+      email: site.email,
+      senha: site.senha,
+      url: site.url,
+      telefone: site.telefone
+    });
+    this.isLoadingResults = false;
+    console.debug('No issues, I will wait until promise is resolved..');
+  }
+
+  onFormSubmit(form:NgForm) {
+    this.isLoadingResults = true;
+    this.api.updateSite(this.id, form)
+      .subscribe(res => {
+          let id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/site-detalhes', id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
+
+  siteDetalhes() {
+    this.router.navigate(['/site-detalhes', this.id]);
   }
 
 }
