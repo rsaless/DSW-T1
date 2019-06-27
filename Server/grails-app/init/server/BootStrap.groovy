7af6fd75fb8801222d2124880dbe21e782fa0064
Rafael Sales Pavarina <rspavarina@gmail.com>
2019-06-27 03:49:54 -0300

    Refatorando o back-end de Teatro

--- Server/grails-app/init/server/BootStrap.groovy
+++ Server/grails-app/init/server/BootStrap.groovy
@@ -7,13 +7,17 @@ import auth.Role
 
 class BootStrap {
 
-    def init = { servletContext ->        
+    def init = { servletContext ->     
+        
+        def teatros = new Teatro[12];
+        def sites = new Site[12];
+        def promocoes = new Promocao[12];
+
         // Criando os papeis
         Role papel_admin = new Role(authority: 'ROLE_ADMIN').save()
         Role papel_teatro = new Role(authority: 'ROLE_TEATRO').save()
         Role papel_site = new Role(authority: 'ROLE_SITE').save()
         
-
         // Criando Admins
         for(int i = 1; i <= 5; i++){
             User usuario_admin = new User(
@@ -23,26 +27,22 @@ class BootStrap {
             UserRole.create(usuario_admin, papel_admin, true)
         }
 
-        def teatros = new Teatro[12];
-        def sites = new Site[12];
-        def promocoes = new Promocao[12];
-
         // Criando Teatros
         for(int i = 0; i < 12; i++){
             teatros[i] = new Teatro(
-                email: "teatro" + i + "@gmail.com",
-                senha: "teatro" + i + "_pass",
+                username: "teatro" + i + "@gmail.com",
+                password: "teatro" + i + "_pass",
                 cidade: "Cidade " + i,
                 nome: "Teatro da Cidade" + i,
                 cnpj: "111000000" + i
             )
             teatros[i].save flush: true
 
-            User usuario_teatro = new User(
+            /*User usuario_teatro = new User(
                 username: "teatro" + i + "@gmail.com", 
                 password: "teatro" + i + "_pass"
-            ).save()
-            UserRole.create(usuario_teatro, papel_teatro, true)
+            ).save()*/
+            UserRole.create(teatros[i], papel_teatro, true)
         }
 
         // Criando Sites
